5d22dd8fd55e7fdeaf9fa56accd58d0074aa7c03
Kevin Ryan <kevinryan1973@gmail.com>
2022-11-07 16:27:50 +0000

    add cpp extensions to devcontainer

--- cpp/apps/smoke_test/bdd/pywrap.cc
+++ cpp/apps/smoke_test/bdd/pywrap.cc
@@ -6,7 +6,7 @@ namespace py = pybind11;
 //module name must be same as generated .so file name
 PYBIND11_MODULE(smoke_test_bdd, m) {
 
-    m.doc() = "Python bindings for the messagelib library";
+    m.doc() = "Python bindings for the smoke_test_bdd library";
 
     py::class_<MessageClass>(m, "MessageClass")
     .def(py::init<>())
