7c2278afd8d6cc920dcdbb9e8125418fc1dcbbfb
Kevin Ryan <kevinryan1973@gmail.com>
2022-11-07 12:16:53 +0000

    pybind11 working with python behave assertions

--- cpp/apps/smoke_test/bdd/pywrap.cc
+++ cpp/apps/smoke_test/bdd/pywrap.cc
@@ -3,6 +3,7 @@
 
 namespace py = pybind11;
 
+//module name must be same as generated .so file name
 PYBIND11_MODULE(smoke_test_bdd, m) {
 
     m.doc() = "Python bindings for the messagelib library";
