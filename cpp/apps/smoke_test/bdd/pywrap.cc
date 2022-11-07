e288cc9f8c04e3f8f7661469aca9b3a45bc35916
Kevin Ryan <kevinryan1973@gmail.com>
2022-11-07 10:42:28 +0000

    configure bazel pybind

--- cpp/apps/smoke_test/bdd/pywrap.cc
+++ cpp/apps/smoke_test/bdd/pywrap.cc
@@ -1,3 +1,14 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/eigen.h>
 #include <smoke_test_message.h>
+
+namespace py = pybind11;
+constexpr auto byref = py::return_value_policy::reference_internal;
+
+PYBIND11_MODULE(MyLib, m) {
+    m.doc() = "optional module docstring";
+
+    py::class_<MyClass>(m, "MyClass")
+    .def(py::init<>())
+    .def("get_message", &MyClass::get_message, byref)
+    ;
+}
