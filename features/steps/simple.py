62c1869ce1756390cda7b700f3fb8135e715e78f
Kevin Ryan <kevinryan1973@gmail.com>
2022-11-07 12:15:26 +0000

    pybind11 working with python behave assertions

--- features/steps/simple.py
+++ features/steps/simple.py
@@ -10,7 +10,8 @@ def step_impl(context):
 
 @when('we implement a test')
 def step_impl(context):
-    assert True is not False
+    messageClass = MessageClass()
+    assert messageClass.getMainMessage() == "SMOKE-TEST"
 
 @then('behave will test it for us!')
 def step_impl(context):
