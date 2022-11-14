ccbae18bdc8bf38850c87250d093b472b0498c3f
Kevin Ryan <kevinryan1973@gmail.com>
2022-11-14 05:55:35 +0000

    add joystick control

--- spot_micro/python/scripts/lcd_monitor/lcd_monitor.py
+++ spot_micro/python/scripts/lcd_monitor/lcd_monitor.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from spot_micro.python.scripts.lcd_monitor.src.sm_lcd_driver import main
+from spot_micro.python.scripts.lcd_monitor.src.lcd_driver import main
 
 # Node program, calls main function in spot_micro_simple_command python package
 if __name__ == '__main__':
